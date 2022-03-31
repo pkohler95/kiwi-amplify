eca70c0dcf674825d61f73d2643608ccc02bd09a
Peter Kohler <peterkohler95@gmail.com>
2022-03-30 21:05:56 -0500

    add graph ql

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "kiwiamplify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "videoStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
